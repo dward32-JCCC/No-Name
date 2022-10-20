3cf6eaad1953c3f94fa66d422a5acb5efc1eb259
Daniel Ward (Student) <dward32@stumail.jccc.edu>
2022-10-19 20:06:42 -0500

    changes

--- Reference/Bookstore/Areas/Admin/Models/Operation.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Bookstore.Models
-{
-    public static class Operation
-    {
-        public static bool IsAdd(string action) => action.EqualsNoCase("add");
-        public static bool IsDelete(string action) => action.EqualsNoCase("delete");
-    }
-}
